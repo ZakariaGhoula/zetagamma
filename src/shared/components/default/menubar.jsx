a2607bfdb5d5179665644821577ece1cf5c2557c
r-mach <remimach.pro@gmail.com>
2016-08-11 12:04:24 +0200

    Menu mobile, version mobile et tablettes. A gérer: la vidéo du home sur android/ios.

--- src/shared/components/default/menubar.jsx
+++ src/shared/components/default/menubar.jsx
@@ -18,7 +18,8 @@ export default class Menubar extends React.Component {
             open: false,
             isTop: true,
             overButton: false,
-            videoHeight: 0
+            videoHeight: 0,
+            designMMB: false,
         };
         this.changeDisplay = this.changeDisplay.bind(this);
         this.handleSpy = this.handleSpy.bind(this);
@@ -39,7 +40,7 @@ export default class Menubar extends React.Component {
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        return (  this.props.menubar.is_shown != nextProps.menubar.is_shown || this.state.open !== nextState.open || this.state.isTop !== nextState.isTop || this.state.overButton !== nextState.overButton || this.state.videoHeight !== nextState.videoHeight)
+        return (  this.props.menubar.is_shown != nextProps.menubar.is_shown || this.state.open !== nextState.open || this.state.isTop !== nextState.isTop || this.state.overButton !== nextState.overButton || this.state.videoHeight !== nextState.videoHeight || this.state.designMMB !== nextState.designMMB)
     }
 
     changeDisplay(boolean) {
@@ -58,6 +59,7 @@ export default class Menubar extends React.Component {
                 this.setState({isTop: false});
         }
         if (window.scrollY > (this.state.videoHeight) - 80) {
+            this.setState({designMMB: true});
             if(window.scrollY - this.state.lastY > 0){
                 if(this.state.isShown == true){
                     this.props.actions_menubar.changeDisplayMenubar(false);
@@ -68,6 +70,7 @@ export default class Menubar extends React.Component {
                 }
             }
         }else{
+            this.setState({designMMB: false});
             if(this.state.isShown == false){
                 this.props.actions_menubar.changeDisplayMenubar(true);
             }
@@ -211,20 +214,52 @@ export default class Menubar extends React.Component {
                 float: 'right',
                 width: '40px',
                 height: '40px',
-                background: '#fff',
-                borderRadius: '50%'
+                background: 'rgba(255,255,255, 0)',
+                borderRadius: '50%',
+                transitionDuration: '0.6s',
+                transitionTimingFunction: 'ease-out',
             },
             mobileLTb: {
                 margin: '5px 5px 5px 11px',
                 lineHeight: '2px',
                 width: '15px',
-                border: '1px solid #5a5a5a'
+                border: '1px solid #fff',
+                transitionDuration: '0.6s',
+                transitionTimingFunction: 'ease-out',
+            },
+            mobileOpenDivWrapper: {
+                padding: '0px 0px 0px 0px',
+                width: '100%',
+                zIndex: 200,
+                height: '100%',
+                background: 'rgba(0,0,0,0.8)',
+                transition: '0.6s ease-in-out',
+                position: 'fixed',
+                overflow: 'hidden',
+                display: 'inline-block',
+                textAlign: 'center'
+            },
+            centerLogoStyle: {
+                display: 'inline-block',
+                width: '70px',
+                margin: '1vh auto 15px',
+                border: '8px solid #fff'
+            },
+            mobileMenuItem: {
+                color: '#fff',
+                fontVariant: 'small-caps',
+                fontSize: '28px',
+                lineHeight: '9vh',
+                fontWeight: 500,
+                fontFamily: "'Alegreya Sans', sans-serif",
             },
         };
         var menuShow = this.props.menubar.is_shown == true ? styles.showOn:styles.showOff;
         var stylePrimary = this.state.isTop == true ? styles.primaryTop:styles.primaryNormal;
         var lTbn1 = this.state.overButton == true ? styles.lTb2:styles.lTb1;
         var lTbn2 = this.state.overButton == true ? styles.lTb1:styles.lTb2;
+
+        if (this.props.browser.greaterThan.medium) {
         var NormalMenuBar = (
             <div style={Object.assign(menuShow, styles.divWrapper)}>
                 <Navbar fluid style={Object.assign(styles.primary, stylePrimary)}>
@@ -233,7 +268,7 @@ export default class Menubar extends React.Component {
                             <a><img style={styles.logoBase} src={PUBLIC_IMAGES_PATH + "logo_ZG_normal.png"}/></a>
                         </Navbar.Brand>
                     </Navbar.Header>
-                    <Nav pullRight style={{cursor: 'pointer'}} onClick={ ()=> this.setState({open: !this.state.open})}>
+                    <Nav pullRight style={{cursor: 'pointer'}} onClick={ ()=> this.setState({open: true})}>
                         <NavItem onMouseOver={ ()=> this.setState({overButton: true})}
                                  onMouseOut={ ()=> this.setState({overButton: false})} eventKey={1}>
                             <div style={styles.menuButton}>menu</div>
@@ -253,24 +288,44 @@ export default class Menubar extends React.Component {
                     </Navbar.Header>
                     <Nav pullRight style={styles.popNav}>
                         <ul style={styles.mainMenu}>
-                            <li style={styles.mainItem}>Menu 1</li>
-                            <li style={styles.mainItem}>Menu 2</li>
-                            <li style={styles.mainItem}>Menu 3</li>
-                            <li style={styles.mainItem}>Menu 4</li>
-                            <li style={styles.mainItem}>Menu 5</li>
+                            <li style={styles.mainItem}>Étude de cas</li>
+                            <li style={styles.mainItem}>Expertise</li>
+                            <li style={styles.mainItem}>Contact</li>
                         </ul>
-                        <a style={styles.closeButton} onClick={ ()=> this.setState({open: !this.state.open})}>X</a>
+                        <a style={styles.closeButton} onClick={ ()=> this.setState({open: false})}>X</a>
                     </Nav>
                 </Panel>
             </div>
         );
+        } else if (this.props.browser.lessThan.large) {
+            if (this.state.open) {
+                var MobileMenuBar = (
+                    <div style={styles.mobileOpenDivWrapper}>
+                        <div style={{
+                            padding: '28px 27px 0px 0px',
+                            display: 'inline-block',
+                            textAlign: 'right',
+                            width: '100%'
+                        }} onClick={ ()=> this.setState({open: false})}><img style={{width: '18px'}}
+                                                                             src={PUBLIC_IMAGES_PATH + "svg/close.svg"}/>
+                        </div>
+                        <img style={styles.centerLogoStyle} src={PUBLIC_IMAGES_PATH + "logo_ZG.png"}/><br />
+                        <div style={{display: 'block', textAlign: 'center', width: '100%', marginTop: '6vh'}}>
+                            <a style={styles.mobileMenuItem}>Étude de cas</a><br />
+                            <a style={styles.mobileMenuItem}>Expertise</a><br />
+                            <a style={styles.mobileMenuItem}>Contact</a><br />
+                        </div>
+                    </div>
+                );
+            } else {
         var MobileMenuBar = (
             <div style={styles.mobileDivWrapper}>
                 <Navbar fluid style={styles.mobilePrimary}>
-                    <Nav pullRight style={{cursor: 'pointer'}} onClick={ ()=> this.setState({open: !this.state.open})}>
+                    <Nav pullRight style={{cursor: 'pointer'}} onClick={ ()=> this.setState({open: true})}>
                         <NavItem eventKey={1}>
-                            <div style={styles.mobileLittleTweak}>
-                                <hr style={styles.mobileLTb}/>
+                            <div
+                                style={Object.assign(styles.mobileLittleTweak, this.state.designMMB && {background: '#e4e4e4'})}>
+                                <hr style={Object.assign(styles.mobileLTb, this.state.designMMB && {border: '1px solid #5a5a5a'})}/>
                                 <hr style={styles.mobileLTb}/>
                                 <hr style={styles.mobileLTb}/>
                             </div>
@@ -279,7 +334,8 @@ export default class Menubar extends React.Component {
                 </Navbar>
             </div>
         );
-
+            }
+        }
         return (
             <div>
                 {this.props.browser.lessThan.large && MobileMenuBar}
